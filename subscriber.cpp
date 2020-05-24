4460639a90e85691cb671bd5565daf7f362179eb
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-05-24 15:19:01 +0530

    Add files via upload

---
+++ subscriber.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+  
+}
+
+int main(int argc, char **argv)
+{
+
+
+ // cmd arguments
+  std::cout<<argc<<std::endl;
+  std::cout<<argv[1]<<std::endl;
+  // cmd arguments
+
+
+
+  ros::init(argc, argv, "image_listener");
+  ros::NodeHandle nh;
+  cv::namedWindow("view");
+  // cv::startWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("camera/image", 0, imageCallback);
+  ros::spin();
+  cv::destroyWindow("view");
+}
